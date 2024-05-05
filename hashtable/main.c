bfe53cd8337279f96695aa45d90cce37701747fc
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-05-05 20:14:22 +0200

    Initial Commit

---
+++ hashtable/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hashtable
+//
+//  Created by Andrey Marchenko on 05.05.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
